85741365de2da81e8c55f501648dfc10b937054e
Lvgaoming <lgm@xydata.cn>
2019-08-05 23:28:39 +0800

    使用Antd实现TodoList页面，使用Redux实现TodoList功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,79 +1,112 @@
 import React, {Component, Fragment} from 'react';
-import TodoItem from './TodoItem'
-import './style.css';
+import 'antd/dist/antd.css';
+import {Input, Button,List, Typography} from 'antd';
+import store from './store/index';
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputValue: '',
-      list: []
-
-    }
+    console.log(store.getState());
+    this.state = store.getState();
+    store.subscribe(()=>this.handleStoreChange());
+    // this.state = {
+    //   inputValue: '',
+    //   list: []
+    //
+    // }
 
   }
 
 
   render() {
+    console.log('render');
     return (
         <Fragment>
-          <div>
+          <div style={{marginLeft:'50px' ,marginTop:'10px'}}>
             {/*下面是一个input框*/}
-            <label htmlFor="insertArea">输入内容</label>
-            <input
+            {/*<label htmlFor="insertArea">输入内容</label>*/}
+            <Input
+                placeholder="Todo Info"
+                style={{width: '300px',marginRight:'10px'}}
                 id="insertArea"
-                className="input"
                 value={this.state.inputValue}
                 onChange={this.handleInputChange.bind(this)}
                 onKeyDown={this.handleKeyDown.bind(this)}
             />
-            <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+            <Button type="primary" onClick={()=>this.handleBtnClick()}>提交</Button>
+
+            <List
+
+                style = {{marginTop: '10px' ,width:'300px'}}
+                bordered
+                dataSource={this.state.list}
+                renderItem={(item,index) => (
+                    <List.Item onClick={()=>this.handleItemDlete(index)}>
+                      <Typography.Text mark>[进行中]</Typography.Text> {item}
+                    </List.Item>
+                )}
+            />
           </div>
-          <ul>
-            {
-              this.state.list.map((item, index) => {
-                return (
-                    this.renderTodoItem(item, index)
-                )
-              })
-            }
-          </ul>
+          {/*<ul>*/}
+            {/*{*/}
+              {/*this.state.list.map((item, index) => {*/}
+                {/*return (*/}
+                    {/*this.renderTodoItem(item, index)*/}
+                {/*)*/}
+              {/*})*/}
+            {/*}*/}
+          {/*</ul>*/}
+
+
         </Fragment>
     )
   }
 
-  renderTodoItem(item, i) {
-    return (
-        <TodoItem
-            key = {i}
-            content={item}
-            deleteItem={() => this.handleItemDlete(i)}
-        />
-    );
-  }
+  // renderTodoItem(item, i) {
+  //   return (
+  //       <TodoItem
+  //           key={i}
+  //           content={item}
+  //           deleteItem={() => this.handleItemDlete(i)}
+  //       />
+  //   );
+  // }
 
   handleInputChange(e) {
-    const value = e.target.value;
-    this.setState(()=>({
-      inputValue: value
-    }))
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    };
+    store.dispatch(action);
+    // const value = e.target.value;
+    // this.setState(() => ({
+    //   inputValue: value
+    // }))
   }
 
   handleBtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
-      inputValue: ''
-    })
+    const action = {
+      type: 'add_todo_item',
+
+    };
+    store.dispatch(action);
+
   }
+
+  handleStoreChange() {
+    console.log('store change');
+    this.setState(store.getState())
+  }
+
 
   handleItemDlete(index) {
 
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState(()=>({
-      list: list,
-    }));
-    console.log(index)
+    const action = {
+      type: 'delete_todo_item',
+      value: index
+    };
+    store.dispatch(action);
+
   }
 
 
